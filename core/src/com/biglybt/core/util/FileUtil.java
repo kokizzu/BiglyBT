0c2938344b5767dc340e4abe8c3b74864bb89216
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2018-07-06 16:26:08 -0700

    FileTime is minSDK 26

--- core/src/com/biglybt/core/util/FileUtil.java
+++ core/src/com/biglybt/core/util/FileUtil.java
@@ -27,10 +27,12 @@ import java.net.URI;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+/* minSDK 26 
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
+*/
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -1696,6 +1698,7 @@ public class FileUtil {
    		FileFilter  		file_filter,
    		ProgressListener	pl )
     {
+    /* FileTime minSDK 26 
 		FileTime from_last_modified = null;
 		FileTime from_last_access	= null;
 		FileTime from_created		= null;
@@ -1711,9 +1714,11 @@ public class FileUtil {
 			
 		}catch( Throwable e ){
 		}
+		*/
    		
     	boolean result = renameFileSupport( from_file, to_file, fail_on_existing_directory, file_filter, pl );
 
+    /* FileTime minSDK 26 
     	if ( result ){
 
     			// try to maintain the file times if they now differ 
@@ -1738,6 +1743,7 @@ public class FileUtil {
     		}catch( Throwable e ){
     		}
     	}
+    	*/
 
     	return( result );
     }
