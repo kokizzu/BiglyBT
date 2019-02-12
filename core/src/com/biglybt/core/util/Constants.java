ccf3c47238e21778abb1751b44694864ad814a5a
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2019-02-12 10:11:38 -0800

    Android incremental version scheme "_A"

--- core/src/com/biglybt/core/util/Constants.java
+++ core/src/com/biglybt/core/util/Constants.java
@@ -150,7 +150,7 @@ Constants
 	public static final String BIGLY_PEER_ID						= "BI";
 
 	
-	public static final String BIGLYBT_VERSION  = "2.2.0.3_CVS";
+	public static final String BIGLYBT_VERSION  = "2.2.0.3_A09";
 	
 	//public static final String BUILD_VERSION  = "@build.version@";   //Ant replace - believed dead
 	public static final String SUBVERSION		= "";
